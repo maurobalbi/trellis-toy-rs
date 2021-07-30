64f43fc417f251ff2e05dd1231ae4ed33f1dc6ce
mauro <maurobalbi@bluewin.ch>
2021-07-30 18:40:10 +0200

    Add MenuComponent with Rc<RefCell>> SubComponents

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,31 @@
 #![no_std]
 #![no_main]
+#![feature(alloc_error_handler)]
 
+extern crate alloc;
 extern crate feather_m4 as hal;
 extern crate panic_halt;
 extern crate shared_bus;
 
+use alloc::boxed::Box;
+use alloc::vec;
+use alloc::rc::Rc;
+use alloc_cortex_m::CortexMHeap;
+use core::alloc::Layout;
+use core::cell::RefCell;
+
+use embedded_hal::blocking::delay::DelayMs;
 use hal::entry;
-use embedded_hal::blocking::delay::{DelayMs};
 
 mod init;
 
 use init::init;
-use neotrellis;
-use neotrellis::{Color, Event, MultiEvent};
+use neotrellis::{Color, Event, MultiEvent, MultiTrellis, NeoTrellis};
 
 use core::convert::TryInto;
+
+#[global_allocator]
+static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
 
 fn wheel(pos: u8) -> Color {
   if pos < 85 {
@@ -28,30 +39,43 @@ fn wheel(pos: u8) -> Color {
 
 #[entry]
 fn main() -> ! {
+  let start = cortex_m_rt::heap_start() as usize;
+  let size = 2048; // in bytes
+  unsafe { ALLOCATOR.init(start, size) }
+
   let (i2c, mut delay) = init();
 
   let bus = shared_bus::BusManagerSimple::new(i2c);
 
-  // let mut neo = neotrellis::NeoTrellis::new(bus.acquire_i2c(), 0x2f, &mut delay).unwrap();
+  let paint = Rc::new(RefCell::new(PaintComponent {steps: [[0; 8]; 8]}));
 
-  let mut paint = PaintComponent { steps: [[0; 8]; 8] };
+  let mut menu = MenuComponent {
+    current_component: None,
+    exit: Exit {
+      elapsed: 0,
+      count: 0
+    },
+    components: vec![paint],
+  };
 
-  let mut multi = neotrellis::MultiTrellis {
+  let mut multi = MultiTrellis {
     trellis: &mut [
       &mut [
-        neotrellis::NeoTrellis::new(bus.acquire_i2c(), 0x31, &mut delay).unwrap(),
-        neotrellis::NeoTrellis::new(bus.acquire_i2c(), 0x30, &mut delay).unwrap(),
+        NeoTrellis::new(bus.acquire_i2c(), 0x31, &mut delay).unwrap(),
+        NeoTrellis::new(bus.acquire_i2c(), 0x30, &mut delay).unwrap(),
       ],
       &mut [
-        neotrellis::NeoTrellis::new(bus.acquire_i2c(), 0x2E, &mut delay).unwrap(),
-        neotrellis::NeoTrellis::new(bus.acquire_i2c(), 0x2F, &mut delay).unwrap(),
+        NeoTrellis::new(bus.acquire_i2c(), 0x2E, &mut delay).unwrap(),
+        NeoTrellis::new(bus.acquire_i2c(), 0x2F, &mut delay).unwrap(),
       ],
     ],
   };
 
   for y in 0..8 {
     for x in 0..8 {
-      multi.set_led_color((x, y), wheel((x + 8 * y) * 4), &mut delay).unwrap();
+      multi
+        .set_led_color((x, y), wheel((x + 8 * y) * 4), &mut delay)
+        .unwrap();
       multi.show(&mut delay).unwrap();
       delay.delay_ms(45 as u32);
     }
@@ -59,7 +83,9 @@ fn main() -> ! {
 
   for y in 0..8 {
     for x in 0..8 {
-      multi.set_led_color((x, y), Color::rgb(0, 0, 0), &mut delay).unwrap();
+      multi
+        .set_led_color((x, y), Color::rgb(0, 0, 0), &mut delay)
+        .unwrap();
       multi.show(&mut delay).unwrap();
       delay.delay_ms(45 as u32);
     }
@@ -72,12 +98,12 @@ fn main() -> ! {
     for event in &mut *events {
       match event {
         Some(event) => {
-          paint.update(*event);
+          menu.update(*event);
         }
         _ => (),
       }
     }
-    let colors = paint.render();
+    let colors = menu.render();
     for (x, row) in colors.iter().enumerate() {
       for (y, color) in row.iter().enumerate() {
         match color {
@@ -85,11 +111,7 @@ fn main() -> ! {
             .set_led_color((x as u8, y as u8), *c, &mut delay)
             .unwrap(),
           None => multi
-            .set_led_color(
-              (x as u8, y as u8),
-              neotrellis::Color::rgb(0, 0, 0),
-              &mut delay,
-            )
+            .set_led_color((x as u8, y as u8), Color::rgb(0, 0, 0), &mut delay)
             .unwrap(),
         };
       }
@@ -98,13 +120,48 @@ fn main() -> ! {
   }
 }
 
-struct PaintComponent {
-  steps: [[u8; 8]; 8],
-}
-
 pub trait Component {
   fn update(&mut self, event: MultiEvent) -> ();
-  fn render(&self) -> [[Option<neotrellis::Color>; 8]; 8];
+  fn render(&self) -> [[Option<Color>; 8]; 8];
+}
+
+struct Exit {
+  elapsed: usize,
+  count: u8,
+}
+
+struct MenuComponent {
+  exit: Exit,
+  current_component: Option<Rc<RefCell<dyn Component>>>,
+  components: vec::Vec<Rc<RefCell<dyn Component>>>,
+}
+
+impl Component for MenuComponent {
+  fn update(&mut self, event: MultiEvent) -> () {
+    if let MultiEvent {
+      coordinate: (0, 0),
+      event: Event::Rising,
+    } = event
+    {
+      self.current_component = Some(Rc::clone(&self.components[0]));
+    }
+
+    match &self.current_component {
+      Some(component) => component.borrow_mut().update(event),
+      None => ()
+    }
+  }
+
+  fn render(&self) -> [[Option<Color>; 8]; 8] {
+    match &self.current_component {
+      Some(component) => component.borrow().render(),
+      None => [[Some(Color::rgb(0, 0, 10)); 8]; 8]
+    }
+  }
+}
+
+struct PaintComponent {
+  steps: [[u8; 8]; 8],
 }
 
 impl Component for PaintComponent {
@@ -126,14 +183,14 @@ impl Component for PaintComponent {
     ()
   }
 
-  fn render(&self) -> [[Option<neotrellis::Color>; 8]; 8] {
-    let mut colors: [[Option<neotrellis::Color>; 8]; 8] = [[None; 8]; 8];
+  fn render(&self) -> [[Option<Color>; 8]; 8] {
+    let mut colors: [[Option<Color>; 8]; 8] = [[None; 8]; 8];
     for (x, item) in self.steps.iter().enumerate() {
       for (y, step) in item.iter().enumerate() {
         match step {
-          1 => colors[x][y] = Some(neotrellis::Color::rgb(125, 0, 0)),
-          2 => colors[x][y] = Some(neotrellis::Color::rgb(0, 125, 0)),
-          3 => colors[x][y] = Some(neotrellis::Color::rgb(0, 0, 125)),
+          1 => colors[x][y] = Some(Color::rgb(125, 0, 0)),
+          2 => colors[x][y] = Some(Color::rgb(0, 125, 0)),
+          3 => colors[x][y] = Some(Color::rgb(0, 0, 125)),
           _ => (),
         }
       }
@@ -141,4 +198,9 @@ impl Component for PaintComponent {
 
     colors
   }
+}
+
+#[alloc_error_handler]
+fn oom(_: Layout) -> ! {
+  loop {}
 }
