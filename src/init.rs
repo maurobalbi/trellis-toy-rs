ab50344df77f6b2295b29300cfbe2005ade89033
mauro <maurobalbi@bluewin.ch>
2021-07-31 12:29:26 +0200

    Add usb debugging

--- src/init.rs
+++ src/init.rs
@@ -1,42 +1,131 @@
 use hal::clock::GenericClockController;
 use hal::delay::Delay;
 use hal::i2c_master;
-use hal::pac::{CorePeripherals, Peripherals};
+use hal::pac::{interrupt, CorePeripherals, Peripherals};
 use hal::time::Hertz;
+use hal::usb::UsbBus;
+
+use alloc::string::String;
+
+use usb_device::bus::UsbBusAllocator;
+
+use usb_device::prelude::*;
+use usbd_serial::{SerialPort, USB_CLASS_CDC};
 
 use feather_m4::sercom::I2CMaster2;
+use cortex_m::peripheral::NVIC;
+
 
 pub fn init<'a>() -> (
-    I2CMaster2<
-        hal::sercom::Sercom2Pad0<hal::gpio::Pa12<hal::gpio::PfC>>,
-        hal::sercom::Sercom2Pad1<hal::gpio::Pa13<hal::gpio::PfC>>,
-    >,
-    Delay,
+  I2CMaster2<
+    hal::sercom::Sercom2Pad0<hal::gpio::Pa12<hal::gpio::PfC>>,
+    hal::sercom::Sercom2Pad1<hal::gpio::Pa13<hal::gpio::PfC>>,
+  >,
+  Delay,
 ) {
-    let mut peripherals = Peripherals::take().unwrap();
-    let core = CorePeripherals::take().unwrap();
-    let mut clocks = GenericClockController::with_external_32kosc(
-        peripherals.GCLK,
-        &mut peripherals.MCLK,
-        &mut peripherals.OSC32KCTRL,
-        &mut peripherals.OSCCTRL,
-        &mut peripherals.NVMCTRL,
+  let mut peripherals = Peripherals::take().unwrap();
+  let mut core = CorePeripherals::take().unwrap();
+  let mut clocks = GenericClockController::with_external_32kosc(
+    peripherals.GCLK,
+    &mut peripherals.MCLK,
+    &mut peripherals.OSC32KCTRL,
+    &mut peripherals.OSCCTRL,
+    &mut peripherals.NVMCTRL,
+  );
+  let mut pins = hal::Pins::new(peripherals.PORT);
+  let delay = Delay::new(core.SYST, &mut clocks);
+
+  let bus_allocator = unsafe {
+    USB_ALLOCATOR = Some(hal::usb_allocator(
+      pins.usb_dm,
+      pins.usb_dp,
+      peripherals.USB,
+      &mut clocks,
+      &mut peripherals.MCLK,
+    ));
+    USB_ALLOCATOR.as_ref().unwrap()
+  };
+
+  unsafe {
+    USB_SERIAL = Some(SerialPort::new(&bus_allocator));
+    USB_BUS = Some(
+      UsbDeviceBuilder::new(&bus_allocator, UsbVidPid(0x16c0, 0x27dd))
+        .manufacturer("Fake company")
+        .product("Serial port")
+        .serial_number("TEST")
+        .device_class(USB_CLASS_CDC)
+        .build(),
     );
-    let mut pins = hal::Pins::new(peripherals.PORT);
-    let delay = Delay::new(core.SYST, &mut clocks);
+  }
 
-    let sda = pins.sda.into_floating_input(&mut pins.port);
-    let scl = pins.scl.into_floating_input(&mut pins.port);
+  unsafe {
+    core.NVIC.set_priority(interrupt::USB_OTHER, 1);
+    core.NVIC.set_priority(interrupt::USB_TRCPT0, 1);
+    core.NVIC.set_priority(interrupt::USB_TRCPT1, 1);
+    NVIC::unmask(interrupt::USB_OTHER);
+    NVIC::unmask(interrupt::USB_TRCPT0);
+    NVIC::unmask(interrupt::USB_TRCPT1);
+  }
 
-    let i2c = i2c_master(
-        &mut clocks,
-        Hertz(100_000),
-        peripherals.SERCOM2,
-        &mut peripherals.MCLK,
-        sda,
-        scl,
-        &mut pins.port,
-    );
+  let sda = pins.sda.into_floating_input(&mut pins.port);
+  let scl = pins.scl.into_floating_input(&mut pins.port);
 
-    (i2c, delay)
+  let i2c = i2c_master(
+    &mut clocks,
+    Hertz(100_000),
+    peripherals.SERCOM2,
+    &mut peripherals.MCLK,
+    sda,
+    scl,
+    &mut pins.port,
+  );
+
+  (i2c, delay)
+}
+
+static mut USB_ALLOCATOR: Option<UsbBusAllocator<UsbBus>> = None;
+static mut USB_BUS: Option<UsbDevice<UsbBus>> = None;
+static mut USB_SERIAL: Option<SerialPort<UsbBus>> = None;
+
+pub fn debug(mut text: String) {
+  text.push('\r');
+  text.push('\n');
+  unsafe {
+    USB_SERIAL.as_mut().map(|serial| serial.write(text.as_bytes())).unwrap();
+  }
+}
+
+fn poll_usb() {
+    unsafe {
+        USB_BUS.as_mut().map(|usb_dev| {
+            USB_SERIAL.as_mut().map(|serial| {
+                usb_dev.poll(&mut [serial]);
+                let mut buf = [0u8; 64];
+
+                if let Ok(count) = serial.read(&mut buf) {
+                    for (i, c) in buf.iter().enumerate() {
+                        if i >= count {
+                            break;
+                        }
+                        serial.write(b"mauro").unwrap();
+                    }
+                };
+            });
+        });
+    };
+}
+
+#[interrupt]
+fn USB_OTHER() {
+    poll_usb();
+}
+
+#[interrupt]
+fn USB_TRCPT0() {
+    poll_usb();
+}
+
+#[interrupt]
+fn USB_TRCPT1() {
+    poll_usb();
 }
---
+++ src/menu.rs
@@ -0,0 +1,104 @@
+use alloc::rc::Rc;
+use alloc::vec;
+
+use crate::init::debug;
+use core::cell::RefCell;
+use core::convert::TryInto;
+
+use neotrellis::{Color, Event, MultiEvent};
+
+pub trait Component {
+  fn update(&mut self, event: MultiEvent) -> ();
+  fn render(&self) -> [[Option<Color>; 8]; 8];
+}
+
+struct Exit {
+  elapsed: usize,
+  count: u8,
+}
+
+pub struct MenuComponent {
+  exit: Exit,
+  current_component: Option<Rc<RefCell<dyn Component>>>,
+  components: vec::Vec<Rc<RefCell<dyn Component>>>,
+}
+
+impl MenuComponent {
+  pub fn new() -> Self {
+    let paint = Rc::new(RefCell::new(PaintComponent { steps: [[0; 8]; 8] }));
+
+    Self {
+      current_component: None,
+      exit: Exit {
+        elapsed: 0,
+        count: 0,
+      },
+      components: vec![paint],
+    }
+  }
+}
+
+impl Component for MenuComponent {
+  fn update(&mut self, event: MultiEvent) -> () {
+    if let MultiEvent {
+      coordinate: (0, 0),
+      event: Event::Rising,
+    } = event
+    {
+      self.current_component = Some(Rc::clone(&self.components[0]));
+    }
+
+    match &self.current_component {
+      Some(component) => component.borrow_mut().update(event),
+      None => (),
+    }
+  }
+
+  fn render(&self) -> [[Option<Color>; 8]; 8] {
+    match &self.current_component {
+      Some(component) => component.borrow().render(),
+      None => [[Some(Color::rgb(0, 0, 10)); 8]; 8],
+    }
+  }
+}
+
+struct PaintComponent {
+  steps: [[u8; 8]; 8],
+}
+
+impl Component for PaintComponent {
+  fn update(&mut self, event: MultiEvent) -> () {
+    if let MultiEvent {
+      coordinate: (x, y),
+      event: Event::Rising,
+    } = event
+    {
+      debug(format!("abc {}", 1));
+      let xi: usize = x.try_into().unwrap();
+      let yi: usize = y.try_into().unwrap();
+      let step = &mut self.steps[xi][yi];
+      if *step == 3 {
+        *step = 0;
+      } else {
+        *step += 1;
+      }
+    }
+    ()
+  }
+
+  fn render(&self) -> [[Option<Color>; 8]; 8] {
+    let mut colors: [[Option<Color>; 8]; 8] = [[None; 8]; 8];
+    for (x, item) in self.steps.iter().enumerate() {
+      for (y, step) in item.iter().enumerate() {
+        match step {
+          1 => colors[x][y] = Some(Color::rgb(125, 0, 0)),
+          2 => colors[x][y] = Some(Color::rgb(0, 125, 0)),
+          3 => colors[x][y] = Some(Color::rgb(0, 0, 125)),
+          _ => (),
+        }
+      }
+    }
+
+    colors
+  }
+}
